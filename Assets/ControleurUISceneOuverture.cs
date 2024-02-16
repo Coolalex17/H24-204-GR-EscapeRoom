335dd3b0223b084fe5b8d480fb29f20372972b8e
ManeDavtyan5 <mane.davtyan05@gmail.com>
2024-02-16 12:41:29 -0500

    Début du menu principal

--- Assets/ControleurUISceneOuverture.cs
+++ Assets/ControleurUISceneOuverture.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class ControleurInterface : MonoBehaviour
+public class ControleurUISceneOuverture : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-    //panel
+        //panel
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void Btn1()
+    
+    public void BtnContinuer()
     {
-       //panel.GetComponent<Image>().color = Color.white;
-        SceneManager.LoadScene("SceneOuverture");
+        SceneManager.LoadScene("ScenePrincipale");
     }
 
+
 }
+
+
