918c38e4f7cc359c0c518b6e4c4952c476248427
Coolalex17 <87104665+Coolalex17@users.noreply.github.com>
2024-02-16 12:35:05 -0500

    Ajout dune scene d'option qui peut modifier la sensibilitee de la camera du joueur

--- Assets/Scripts/Joueur/Controle Camera.cs
+++ Assets/Scripts/Joueur/Controle Camera.cs
@@ -8,7 +8,6 @@ public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private Camera CameraJoueur;
     [SerializeField] private Transform Joueur;
-    private float sensitivite = 1;
     private float RotationVerticale = 0;
     private readonly float DISTANCE_INTERACTION = 10;
 
@@ -21,10 +20,10 @@ public class NewBehaviourScript : MonoBehaviour
     void FixedUpdate()
     {
 
-        RotationVerticale += Input.GetAxis("Mouse Y") * -sensitivite;
+        RotationVerticale += Input.GetAxis("Mouse Y") * -PreferencesJoueur.ObtenirSensibiliteeCamera();
         RotationVerticale = Mathf.Clamp(RotationVerticale, -100, 100);
 
-        Joueur.transform.Rotate(Vector3.down * Input.GetAxis("Mouse X") * -sensitivite);
+        Joueur.transform.Rotate(Vector3.down * Input.GetAxis("Mouse X") * -PreferencesJoueur.ObtenirSensibiliteeCamera());
 
 
         transform.localRotation = Quaternion.Euler(RotationVerticale,0f, 0f);
