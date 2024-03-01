22a8289309c4a2a8f9b430b182e625d848d1e9d3
Coolalex17 <87104665+Coolalex17@users.noreply.github.com>
2024-03-01 12:10:43 -0500

    Creation du projectile
    le projectile qui devra orbiter la planete va etre tirer, les physiques nont pas encore ete implementee

--- Assets/Scripts/Jeux/JeuPhysique/ControleurJeuPhysique.cs
+++ Assets/Scripts/Jeux/JeuPhysique/ControleurJeuPhysique.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
 using UnityEngine.UIElements;
 
 public class ControleurJeuPhysique : MonoBehaviour
@@ -12,13 +13,17 @@ public class ControleurJeuPhysique : MonoBehaviour
     [SerializeField] private Transform conteneurProjectiles;
     [SerializeField] private Transform fleche;
 
+   
+
 
     private Slider sliderForce;
     private Slider sliderAngle;
     private Button boutonTirer;
     private bool jeuDebute;
+
     void Start()
     {
+        projectileEnVie = false;
         sliderForce = UIJeu.rootVisualElement.Q<Slider>("SliderForce");
         sliderAngle = UIJeu.rootVisualElement.Q<Slider>("SliderRotation");
         boutonTirer = UIJeu.rootVisualElement.Q<Button>("BoutonTirer");
@@ -42,11 +47,37 @@ public class ControleurJeuPhysique : MonoBehaviour
             conteneurProjectiles.rotation = Quaternion.Euler(0f, 0f, -sliderAngle.value);
             fleche.localScale =new Vector3(1, (sliderForce.value/100) + 0.5f, 1);
             fleche.localPosition = new Vector3(0, (sliderForce.value / 100 + 1.6f) / 2 + 0.2f, 0);
+
+            if (projectileEnVie)
+            {
+                bougerProjectile();
+            }
         }
+        
+        
     }
+
+    [SerializeField] private GameObject projectile;
+    private bool projectileEnVie;
+    private float vitesseX;
+    private float vitesseY;
+    private float gravitee;
+    private void bougerProjectile()
+    {
+        projectile.transform.position += new Vector3(vitesseX,vitesseY,0)* Time.deltaTime;   
+
+    }
+
     private void tirer(ClickEvent ev)
     {
-
+        projectileEnVie = true;
+        projectile = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        projectile.transform.SetParent(conteneurProjectiles, false);
+        projectile.transform.localPosition = new Vector3(0,2,0);
+        projectile.transform.rotation = Quaternion.Euler(90, 0, 0);
+        vitesseY = 1;
+        vitesseX = 1;
+        //fleche.gameObject.
     }
 
 }
