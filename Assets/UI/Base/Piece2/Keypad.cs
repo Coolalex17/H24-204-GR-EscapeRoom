8b58afe9ba587dca6515e072ddd0779ff33ea2cc
kaorin <kaorimela@gmail.com>
2024-04-21 20:33:19 -0400

    Clavier
    Les modifications sont celles apportées pour le clavier qui permet d'ouvrir le réfrigérateur de la pièce 2 pour obtenir une clé

---
+++ Assets/UI/Base/Piece2/Keypad.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=TO0g5jyjpYU
+    private string reponse = "7883";
+    [SerializeField] private Text Ans;
+    [SerializeField] private Animator Refregirateur;
+
+
+
+    public void Nombre(int nombre)
+    {
+        if (Ans.text== "Incorrect" || Ans.text == "Correct"){
+            Ans.text = "" ;
+        }
+        Ans.text += nombre.ToString();
+    }
+    public void Execute()
+    {
+        if (Ans.text == reponse)
+        {
+            Ans.text = "Correct";
+            Refregirateur.SetBool("Ouvert", true);
+            StartCoroutine("Stop");
+        } else
+        {
+            Ans.text = "Incorrect";
+        }
+    }
+    IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Refregirateur.SetBool("Ouvert", false);
+        Refregirateur.enabled = false;
+    }
+}
+
