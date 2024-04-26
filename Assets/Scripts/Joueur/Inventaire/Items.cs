08f2065280d493bed52efb1ce9d6b18b80c46624
Humi852 <humaira_moulvi@hotmail.com>
2024-04-26 10:32:51 -0400

    3emJeu(Terminé)
    Reste à finaliser la texture du ballon, et probablement rajouter un autre bouton ''Quitter'' lorsque le joueur atteint la scène finale (où le joueur à l'option de rejouer le jeux).

--- Assets/Scripts/Joueur/Inventaire/Items.cs
+++ Assets/Scripts/Joueur/Inventaire/Items.cs
@@ -1,42 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public partial class Inventaire
 {
-    
-
     public enum Items
     {
         CLEE_PORTE1,
-        CLEE_PORTE2,
-
+        CLEE_PORTE2
     }
-    
-    
-    private Dictionary<Items, Texture> DictionaireImage;
 
+    private Dictionary<Items, Texture> DictionaireImage;
+    private Texture[] Images_ = new Texture[2]; // Assuming you need at least 2 images
 
     private void AssocierImages()
     {
         DictionaireImage = new Dictionary<Items, Texture>();
 
-        DictionaireImage.Add(Items.CLEE_PORTE1, Images[0]);
-        DictionaireImage.Add(Items.CLEE_PORTE2, Images[1]);
-
-
+        DictionaireImage.Add(Items.CLEE_PORTE1, Images_[0]);
+        DictionaireImage.Add(Items.CLEE_PORTE2, Images_[1]);
     }
+
     public Texture ObtenirImage(Items item)
     {
         Texture image = null;
         DictionaireImage.TryGetValue(item, out image);
         return image;
     }
-
-
-
 }
+
 
